c8d551b2f9c79625c126809fb6e234c77927bd7b
chogomo <j0921352@gmail.com>
2022-01-22 16:51:29 +0800

    當前資料更新

---
+++ tool_escape.js
@@ -0,0 +1,14 @@
+window.onload=function () {
+    var main_wrap = document.getElementById("main_wrap");
+
+    //取得視窗寬度=1920 高度=937
+    // main_wrap.onclick=function () {
+    //     var h = window.innerHeight;
+    //     var w = window.innerWidth;
+    //     console.log(h);
+    //     console.log(w);
+    // };
+
+
+
+};
